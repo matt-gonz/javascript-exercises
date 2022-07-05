93253c3a083316f5ad5f07badd14e58afb705036
Matthew <matthewgonzalez2324@gmail.com>
2022-07-04 22:37:18 -0700

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const myArray = args[0];
 
+    const newArray = [];
+
+    myArray.forEach((item) => {
+        if (!args.includes(item)){
+            newArray.push(item);
+        }
+    })
+
+    return newArray;
 };
 
 // Do not edit below this line
