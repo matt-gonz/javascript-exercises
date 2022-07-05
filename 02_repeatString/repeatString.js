82a91fdd38b0c89273393a8cf03553e7d21e5d96
Matthew <matthewgonzalez2324@gmail.com>
2022-07-04 22:07:29 -0700

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(myString, myInt) {
+    if (myInt < 0) return 'ERROR';
+    let repeatedString = '';
+    for (let i=0; i<myInt; i++){
+        repeatedString += myString;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
