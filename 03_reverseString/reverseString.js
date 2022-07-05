7a24f0b4da38d719ea0b1bac8c6487132fbb773d
Matthew <matthewgonzalez2324@gmail.com>
2022-07-04 22:18:31 -0700

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(myString) {
+    let splitString = myString.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
