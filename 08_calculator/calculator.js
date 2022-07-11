1b94b82be233dd299c8d7886e7fc2876db852f12
Matthew <matthewgonzalez2324@gmail.com>
2022-07-11 14:48:03 -0700

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	return myArray.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  return myArray.length 
+    ? myArray.reduce((total, nextItem) => total * nextItem)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0){
+    return 1;
+  }
+	let total = 1;
+  for (let i = n; i > 0; i++){
+    total *= n;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
