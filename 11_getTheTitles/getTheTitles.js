23e503702361cadfdce4640ea86ac685dec900cd
Matthew <matthewgonzalez2324@gmail.com>
2022-07-11 18:22:43 -0700

    Complete get the titles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(myArray) {
+    return myArray.map(book => book.title);
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
